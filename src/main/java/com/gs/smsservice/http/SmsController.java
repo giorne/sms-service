38382c9ac8f2f96707543e18d9830b88ff3f4251
Giorne <giorne.souza@gmail.com>
2018-08-06 14:35:27 -0300

    add swagger configs

--- src/main/java/com/gs/smsservice/http/SmsController.java
+++ src/main/java/com/gs/smsservice/http/SmsController.java
@@ -2,15 +2,22 @@ package com.gs.smsservice.http;
 
 import com.gs.smsservice.handler.SmsHandler;
 import com.gs.smsservice.http.json.SmsRequest;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.ValidationException;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/sms")
+@RequestMapping("/api/sms/commands")
+@Api(tags = "Api for handling sms commands", produces = MediaType.APPLICATION_JSON_VALUE)
 public class SmsController {
 
     private final SmsHandler smsHandler;
@@ -19,9 +26,14 @@ public class SmsController {
         this.smsHandler = smsHandler;
     }
 
+    @ApiOperation(value = "Send a command for a deviceId")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Command executed", response = String.class),
+            @ApiResponse(code = 400, message = "Bad request", response = ValidationException.class)
+    })
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
-    public String post(@RequestBody @Valid SmsRequest request) {
+    public String sendCommand(@RequestBody @Valid final SmsRequest request) {
         log.info("Receiving request: {}", request);
         return smsHandler.handleSmsRequest(request.getSmsContent(), request.getDeviceId());
     }
